cfb5568f5ae8ce2f16d270ce906ea2997cb88f2c
bensauer <ben@redbeard.org.uk>
2017-02-10 21:46:09 +0000

    First commit, basics working

---
+++ readme.md
@@ -0,0 +1,7 @@
+A script to run a Wizard of Oz test from a plain text script on OSX using "say".
+
+USE
+
+1. Edit the text file 'script.txt' with your phrases.
+2. open the Terminal, and use 'cd' to enter the directory where this script is. 
+3. type 'python wizardofoz.py' to run the script.
---
+++ wizardofoz.py
@@ -0,0 +1,82 @@
+import os,sys
+
+def wait_key():
+    ''' Wait for a key press on the console and return it. '''
+    result = None
+    if os.name == 'nt':
+        import msvcrt
+        result = msvcrt.getch()
+    else:
+        import termios
+        fd = sys.stdin.fileno()
+
+        oldterm = termios.tcgetattr(fd)
+        newattr = termios.tcgetattr(fd)
+        newattr[3] = newattr[3] & ~termios.ICANON & ~termios.ECHO
+        termios.tcsetattr(fd, termios.TCSANOW, newattr)
+
+        try:
+            result = sys.stdin.read(1)
+        except IOError:
+            pass
+        finally:
+            termios.tcsetattr(fd, termios.TCSAFLUSH, oldterm)
+
+    return result
+
+def printLines(lines):
+
+	for line in lines:
+		print(line[0] + ". " + line[1] + "\r")
+
+#clear the screen
+os.system("clear")
+
+keys = "1234567890qwertyuiopasdfghjklzxcvbnm"
+keyList = list(keys)
+
+f = open("script.txt")
+# will need to replace this with an argument, or perhaps not to keep command line simple
+
+#lineNo = 1
+linePos = 0
+#lines = []
+lines = []
+keysIndex = []
+
+next = f.readline()
+while next != "":
+	next = next.strip()	
+#	lines.append(next)
+	lines.append([])
+	lines[linePos].append(keyList[linePos])
+	keysIndex.append(keyList[linePos])
+	lines[linePos].append(next)
+
+	#print(str(lineNo) + ". " + next)
+#	lineNo = lineNo + 1
+	linePos = linePos + 1
+	next = f.readline()
+
+#should probably clean up empty array entries
+lineTotal = len(lines)
+#print(lines)
+
+printLines(lines)
+
+#keyItem = keysIndex.index("q")
+
+#print keyItem
+
+#os.system("say " + lines[0])
+
+while True:
+	choice = wait_key()
+	keyItem = keysIndex.index(choice) 
+	#need an if statement here to check if the keypress is in the range
+	os.system("say \"" + lines[keyItem][1] + "\"")
+#	if (choice <= lineTotal)        
+#		os.system("say " + lines[choice])
+ #       break
+
+
